34b57f15b8f4fbf01187e4857ea2a522d34ba536
keshavsharma02 <119558362+keshavsharma02@users.noreply.github.com>
2026-02-15 00:12:45 +0530

    Time: 3 ms (83.76%) | Memory: 9.1 MB (94.86%) - LeetSync

--- 38-count-and-say/count-and-say.cpp
+++ 38-count-and-say/count-and-say.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
     string countAndSay(int n) {
-        string res = "1";   // Base case
+        string s = "1";
 
-        for (int i = 2; i <= n; i++) {
-            string temp = "";
-            int count = 1;
+        while (--n) {
+            string t;
+            t.reserve(s.size() * 2);   // prevent reallocations
 
-            for (int j = 1; j < res.size(); j++) {
-                if (res[j] == res[j - 1]) {
-                    count++;
-                } else {
-                    temp += to_string(count);
-                    temp += res[j - 1];
-                    count = 1;
+            for (int i = 0; i < s.size(); ) {
+                char c = s[i];
+                int cnt = 0;
+
+                // count same chars
+                while (i < s.size() && s[i] == c) {
+                    ++cnt;
+                    ++i;
                 }
+
+                // manually append count (faster than to_string)
+                if (cnt >= 10) t.push_back('0' + cnt / 10);
+                t.push_back('0' + cnt % 10);
+                t.push_back(c);
             }
 
-            // Add last group
-            temp += to_string(count);
-            temp += res.back();
-
-            res = temp;
+            s.swap(t);   // faster than assignment
         }
 
-        return res;
+        return s;
     }
 };
