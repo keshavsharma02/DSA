fa6cdd7f4f5f1645cc8caeb30d623bde158d6f21
keshav sharma <119558362+keshavsharma02@users.noreply.github.com>
2026-05-05 16:15:48 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (31.20%) - LeetSync

--- 61-rotate-list/rotate-list.cpp
+++ 61-rotate-list/rotate-list.cpp
@@ -8,29 +8,39 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        if (head == nullptr) return nullptr;
-        ListNode* tmp = head;
-        int len = 0;
-        while(tmp != nullptr) {
-            len++;
-            tmp = tmp -> next;
-        }
-        tmp = head;
+        if (!head || !head->next || k == 0) return head;
 
-        for(int i = 0; i < (k%len); i++) {
-            ListNode* lastNode = head;
-            tmp = head;
-            while(tmp->next != nullptr){
-                lastNode = tmp;
-               tmp = tmp->next;
-            }
-            lastNode->next = nullptr;
-            tmp->next = head;
-            head = tmp;
+        //find length and last node
+        int n = 1;
+        ListNode* tail = head;
+        while (tail->next) {
+            tail = tail->next;
+            n++;
         }
-    return head;
+
+        // reduce k
+        k = k % n;
+        if (k == 0) return head;
+
+        // make circular
+        tail->next = head;
+
+        // find new tail (n - k - 1 steps)
+        int steps = n - k;
+        ListNode* newTail = head;
+        while (--steps) {
+            newTail = newTail->next;
+        }
+
+        // break circle
+        ListNode* newHead = newTail->next;
+        newTail->next = nullptr;
+
+        return newHead;
     }
 };
